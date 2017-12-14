86e32cc0cb3b86d3db99293332f57f1e12849603
@eduardo_78d <eduardo78d@gmail.com>
2017-12-14 09:32:27 -0600

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,73 @@
+const title = document.getElementById('title');
+title.innerHTML = 'Cursos';
+
+const description = document.getElementById('description');
+description.textContent = 'Listado de cursos';
+
+//const items = document.getElementsByClassName('list-group-item');
+//const items = document.getElementsByTagName('li');
+const items = document.querySelectorAll('li:nth-child(even)');
+for (var i = 0; i < items.length; i++) {
+  let element = items[i];
+  element.style.background = '#f2f2f2';
+}
+
+const row = document.querySelector('.row');
+
+const form = document.getElementById('course-form');
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  let title = document.getElementById('title-form').value;
+  let description = document.getElementById('description-form').value;
+
+  create_card(title, description);
+});
+
+let div = null;
+
+function create_card(title, description){
+
+  div = document.createElement('div');
+  div.className = 'col-sm-6 col-md-4';
+
+  let thumbnail = document.createElement('div');
+  thumbnail.className = 'thumbnail';
+
+  let caption = document.createElement('div');
+  caption.className = 'caption';
+
+  let h3 = document.createElement('h3');
+  h3.textContent = title;
+
+  let p1 = document.createElement('p');
+  p1.textContent = description;
+
+  let p2 = document.createElement('p');
+  let a = document.createElement('a');
+  a.className = 'btn btn-danger';
+  a.textContent = 'Eliminar';
+
+  p2.appendChild(a);
+  caption.appendChild(h3);
+  caption.appendChild(p1);
+  caption.appendChild(p2);
+
+  thumbnail.appendChild(caption);
+  div.appendChild(thumbnail);
+
+  row.appendChild(div);
+}
+
+function create_card_by_innerHTML(title, description){
+  let html = `<div class="col-sm-6 col-md-4">\
+                <div class="thumbnail">\
+                  <div class="caption">\
+                    <h3 id="title_card"> ${title} </h3>\
+                    <p id="description_card"> ${description} </p>\
+                    <p><a href="#" class="btn btn-danger">Acción</a></p>\
+                  </div>\
+                </div>\
+              </div>`;
+    row.innerHTML += html;
+}
